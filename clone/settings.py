f687e1e78061a87c3e4d69bcd38bb6c8089f3490
theclassicwizard <srkjadara@gmail.com>
2020-08-11 16:43:33 +0530

    Used Crispy and Form Bootstrap

--- clone/settings.py
+++ clone/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'quora',
     'social_django',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -136,6 +137,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
